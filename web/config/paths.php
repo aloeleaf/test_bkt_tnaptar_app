e28e8734c5ac4e326c30598ed366235ae06f0fdb
aloeleaf <szabolcs.sandor@gmail.com>
2025-12-17 15:21:01 +0100

    remove phpmyadmimn directory as it doesn't need anymore remove pgadmi from app_base.yml as it doesn't need anymore correct app_base.yml so app will be access from http and https correct paths.php for all protocols work well

--- web/config/paths.php
+++ web/config/paths.php
@@ -1,4 +1,10 @@
 <?php
+// Auto-detect protocol (http or https)
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || 
+            ($_SERVER['SERVER_PORT'] ?? 80) == 443 ||
+            (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+            ? 'https' : 'http';
+
 return [
-    'base_url' => 'http://test_tnaptar.bpkornyekitvsz.birosagiad.hu',
+    'base_url' => $protocol . '://test_tnaptar.bpkornyekitvsz.birosagiad.hu',
 ];
